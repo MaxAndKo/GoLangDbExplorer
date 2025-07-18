3f9483fc0aeb310b64fc3673b2d3531398ef404c
maxandko <avatar32255@yandex.com>
2025-07-18 21:34:35 +0300

    Получаю данные о таблицах и их полях

--- db_explorer.go
+++ db_explorer.go
@@ -9,6 +9,18 @@ import (
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
 
+type FieldMetaData struct {
+	Field      string
+	Type       string
+	Collation  bool
+	Null       string
+	Key        string
+	Default    string
+	Extra      string
+	Privileges string
+	Comment    string
+}
+
 type DbExplorer struct {
 	DB *sql.DB
 }
@@ -18,21 +30,40 @@ func (*DbExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func NewDbExplorer(db *sql.DB) (*DbExplorer, error) {
-	tables, err := db.Query("SHOW TABLES;")
+	tablesRs, err := db.Query("SHOW TABLES;")
 	if err != nil {
 		return nil, err
 	}
 
-	names := make([]string, 0, 0)
-	for tables.Next() {
+	tableNames := make(map[string][]FieldMetaData)
+	for tablesRs.Next() {
 		var name string
-		if err := tables.Scan(&name); err != nil {
+		if err := tablesRs.Scan(&name); err != nil {
 			return nil, err
 		}
-		names = append(names, name)
+
+		tableNames[name] = nil
+	}
+	tablesRs.Close()
+
+	for tableName, _ := range tableNames {
+		fieldsRs, err := db.Query(fmt.Sprintf("SHOW FULL COLUMNS FROM `%s`;", tableName))
+		if err != nil {
+			return nil, err
+		}
+		fieldMetaData := make([]FieldMetaData, 0)
+		for fieldsRs.Next() {
+			field := FieldMetaData{}
+			fieldsRs.Scan(
+				&field.Field, &field.Type, &field.Collation,
+				&field.Null, &field.Key, &field.Default,
+				&field.Extra, &field.Privileges, &field.Comment,
+			)
+			fieldMetaData = append(fieldMetaData, field)
+		}
+		tableNames[tableName] = fieldMetaData
+		fieldsRs.Close()
 	}
 
-	fmt.Println(tables)
-	tables.Close()
 	return &DbExplorer{DB: db}, nil
 }
